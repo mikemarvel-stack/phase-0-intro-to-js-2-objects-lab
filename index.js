c112a11cf0ea021e627975d5dbeb94844abf036b
michael <michaelmuturi15@gmail.com>
2025-04-16 19:11:46 +0300

    added Index.js code

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  // Non-destructive update
+  function updateEmployee(employeeObj, field, newValue) {
+    const updatedEmployee = {
+      ...employeeObj,
+      [field]: newValue
+    };
+    return updatedEmployee;
+  }
+  
+  // Destructive update
+  function overwriteEmployeeInfo(employeeObj, field, newValue) {
+    employeeObj[field] = newValue;
+    return employeeObj;
+  }
+  
+  // Non-destructive delete
+  function removeFieldSafely(emp, fieldToRemove) {
+    const tempEmp = { ...emp };
+    delete tempEmp[fieldToRemove];
+    return tempEmp;
+  }
+  
+  // Destructive delete
+  function stripFieldFromOriginal(emp, fieldToStrip) {
+    delete emp[fieldToStrip];
+    return emp;
+  }
+  // Write your solution in this file!
